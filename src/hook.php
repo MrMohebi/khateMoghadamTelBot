9bc8915ec83aab354a1979e22724c52035c9631c
MrMohebi <MMMohebi@outlook.com>
2021-04-26 17:38:48 +0430

    set input fields

--- src/hook.php
+++ src/hook.php
@@ -1,18 +1,30 @@
 <?php
 // Load composer
 require __DIR__ . '/../vendor/autoload.php';
-
 include_once 'Tokens.php';
+
+use Longman\TelegramBot\Request;
+use Longman\TelegramBot\Telegram;
+
 
 $bot_api_key  = Tokens::$khateMoghadamBot;
 $bot_username = 'khateMoghadamBot';
 
 try {
-    // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+    $telegram = new Telegram($bot_api_key, $bot_username);
 
-    // Handle telegram webhook request
-    $telegram->handle();
+    // input fields
+    $result= json_decode(file_get_contents("php://input"));
+    $userId = isset($result->message->from->id) ? $result->message->from->id : "";
+    $userFirstName =  isset($result->message->from->first_name) ? $result->message->from->first_name : "";
+    $text = isset($result->message->text) ? $result->message->text : "";
+    $chatId = isset($result->message->chat->id) ? $result->message->chat->id : "";
+    $massageId = isset($result->message->message_id) ? $result->message->message_id : "";
+    $idOfRepliedUser = isset($result->message->reply_to_message->from->id) ? $result->message->reply_to_message->from->id : "";
+    $firstname_of_replied_user = isset($result->message->reply_to_message->from->first_name) ? $result->message->reply_to_message->from->first_name : "";
+
+
+
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     // Silence is golden!
     // log telegram errors
