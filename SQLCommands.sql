d42e454f3a7f8a0c7a968d94c890d64728ffa024
MrMohebi <MMMohebi@outlook.com>
2021-04-28 12:41:32 +0430

    add new tables

--- SQLCommands.sql
+++ SQLCommands.sql
@@ -9,3 +9,27 @@ CREATE TABLE `class_reminder`
     `modified_date`         bigint  ,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `anti_spam`
+(
+    `id`                int  AUTO_INCREMENT ,
+    `sender_id`                 tinytext  ,
+    `chat_id`                 tinytext  ,
+    `massage_id`                 tinytext  ,
+    `send_time`                 bigint,
+    `command`                 text,
+    `modified_date`         bigint  ,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `blocked_users`
+(
+    `id`                int  AUTO_INCREMENT ,
+    `user_id`                 tinytext  ,
+    `chat_id`                 tinytext  ,
+    `blocked_time`            bigint,
+    `command`                 text,
+    `reason`           tinytext,
+    `modified_date`         bigint  ,
+    PRIMARY KEY (`id`)
+);
