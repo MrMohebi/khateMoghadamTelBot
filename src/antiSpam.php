386d75caa996b1200ba4cc00e43c8f004b8502b5
MrMohebi <MMMohebi@outlook.com>
2021-04-28 13:51:13 +0430

    add new massage

--- src/antiSpam.php
+++ src/antiSpam.php
@@ -1,29 +1,33 @@
 <?php
 
-use Longman\TelegramBot\Request;
-
-
-
 function antiSpam($senderId, $chatId, $massageId, $text){
     $connTelbot = MysqlConfig::connTelbot();
     $telbotAccess = new MysqldbAccess($connTelbot);
 
-    $spamResponses = array("کیر خر", "fuck u");
+    $spamResponses = array(
+        "ایشون یک کونده اسپمر بلفطره هستن",
+        "مگه ننتو گاییدم که اسپم میکنی بچه کونی",
+        "باشه بابا بیا پولتو بگیر اسپم نکن",
+        "اسپن نکن کیری",
+        "عجب کونده خری ناموسا",
+        "مث ایشون نیاشیم، ایشون یه یه اسپره که میخواد بگه من کون کیدم",
+        "فکر کنم هزار بیشتر بدم خونه هم بیای اسپر کونده",
+        "اعصابم مث فیس این اسپمر کیریه هعی",
+    );
     shuffle($spamResponses);
     $spamResponse = end($spamResponses);
 
-    $timeLimit =  time() - 900; // 15 min
+    $timeLimit =  time() - 8400; // 15 min
     $command = $text;
 
-    $isUserBlocked = count($telbotAccess->select('*', 'blocked_users', "`user_id`='$senderId' AND `chat_id`='$chatId' AND `command`='$command' AND `blocked_time`>=$timeLimit")) > 0;
+    $userBlockedLog =$telbotAccess->select('*', 'blocked_users', "`user_id`='$senderId' AND `chat_id`='$chatId' AND `command`='$command' AND `blocked_time`>=$timeLimit");
 
-    if($isUserBlocked){
-        Request::deleteMessage(['chat_id'=>$chatId, 'message_id' =>$massageId]);
-        return Request::sendMessage(['chat_id'=>$chatId, 'text' =>$spamResponse]);
+    if(count($userBlockedLog) > 1){
+        return $spamResponse;
     }
 
     $massageLogs = $telbotAccess->select('*', 'anti_spam', "`sender_id`='$senderId' AND `chat_id`='$chatId' AND `command`='$command' AND `send_time`>=$timeLimit");
-    if($massageLogs > 10){
+    if(count($massageLogs) > 10){
         $telbotAccess->insert('blocked_users', array(
             'user_id'=>$senderId,
             'chat_id'=>$chatId,
