c5c6efa7b1cde4a31f371adb7d6d2473e3e6750f
MrMohebi <MMMohebi@outlook.com>
2021-07-22 12:39:20 +0430

    base env

--- src/DataAccess/db.config.php
+++ src/DataAccess/db.config.php
@@ -1,13 +1,14 @@
 <?php
-include_once "../Tokens.php";
-
+require __DIR__ . '/../../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../' );
+$dotenv->load();
 
 
 class MysqlConfig {
     protected static string $serverAddr = "127.0.0.1";
 
     public static function connTelbot(){
-        return mysqli_connect(MysqlConfig::$serverAddr, Tokens::mysqliConfigData("username"), Tokens::mysqliConfigData("password"), Tokens::mysqliConfigData("dbName"));
+        return mysqli_connect(MysqlConfig::$serverAddr, $_ENV["MYSQL_USERNAME"], $_ENV["MYSQL_PASSWORD"], $_ENV["MYSQL_DBNAME"]);
     }
 
 }
